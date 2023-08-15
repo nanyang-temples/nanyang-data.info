5a2ee9e447543f941d30fe041c81458022f4a30e
Simon Wiles <simonjwiles@gmail.com>
2023-08-15 10:29:47 -0700

    Convert config file to YAML

--- src/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  title: `Nanyang Sites GIS data pool`,
-  description: `This site makes available data on Chinese religious sites from across Southeast Asia.`,
-  url: `https://nanyang-data.info`, // No trailing slash!
-  shortName: `Nanyang Sites GIS data pool`,
-  dataBasePath: "../data",
-};
--- src/datasets.js
+++ src/datasets.js
@@ -4,20 +4,17 @@ import path from "path";
 import neatCsv from "neat-csv";
 
 import config from "@config";
-const { dataBasePath } = config;
+const {
+  paths: { data: dataPath },
+} = config;
 
-const datasetCsvs = fs
-  .readdirSync(dataBasePath)
-  .filter((f) => f.endsWith(".csv"));
+const datasetCsvs = fs.readdirSync(dataPath).filter((f) => f.endsWith(".csv"));
 
 export const datasets = Object.fromEntries(
   await Promise.all(
     datasetCsvs.map(async (datasetCsvPath) => {
       const id = path.parse(datasetCsvPath).name;
-      const csv = fs.readFileSync(
-        path.join(dataBasePath, datasetCsvPath),
-        "utf-8",
-      );
+      const csv = fs.readFileSync(path.join(dataPath, datasetCsvPath), "utf-8");
       return [id, await neatCsv(csv, "utf-8")];
     }),
   ),
