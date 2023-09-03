c744c737191737fc29fecf7041ac7e8639961e1c
Simon Wiles <simonjwiles@gmail.com>
2023-09-03 13:52:52 -0700

    Add an `md2html` function and pipeline

---
+++ src/lib/markdown-parsing.js
@@ -0,0 +1,15 @@
+import { unified } from "unified";
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+
+export const md2html = async (markdown) => {
+  let processor = unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeStringify, { allowDangerousHtml: true });
+
+  return String(await processor.process(markdown));
+};
