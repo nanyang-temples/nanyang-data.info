7cd0cfa0958d8e79909d25341b8e17eacb60da12
Simon Wiles <simonjwiles@gmail.com>
2023-09-19 10:20:59 -0700

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nanyang-data.info/",
   sitemap: true,
-  integrations: [
-    mdx(),
-    sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [mdx(), sitemap()],
   build: {
     assets: "assets",
   },
