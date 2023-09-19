b6dde5ec7a987c6a8907d78d06ce11ab117cfdba
Simon Wiles <simonjwiles@gmail.com>
2023-09-19 12:17:37 -0700

    Write repo commit hashes into dataset pages

--- src/datasets.js
+++ src/datasets.js
@@ -1,17 +1,27 @@
 import fs from "fs";
 import path from "path";
+import { execSync } from "node:child_process";
 
 import neatCsv from "neat-csv";
 import yaml from "js-yaml";
 
 import config from "@config";
+const repositories = config.datasets;
 const repositoriesPath = config.paths.repositories;
 
 export const datasets = {};
 
-for (let repo of fs.readdirSync(repositoriesPath)) {
-  const repositoryPath = path.join(repositoriesPath, repo);
+for (let repositoryUrl of repositories) {
+  const repositoryName = repositoryUrl.split("/").pop();
+  const repositoryPath = path.join(repositoriesPath, repositoryName);
   if (!fs.lstatSync(repositoryPath).isDirectory()) continue;
+
+  const repositoryRev = execSync("git rev-parse --short HEAD", {
+    cwd: repositoryPath,
+  })
+    .toString()
+    .trim();
+
   const datasetCsvs = fs
     .readdirSync(repositoryPath)
     .filter((f) => f.endsWith(".csv"));
@@ -32,6 +42,14 @@ for (let repo of fs.readdirSync(repositoriesPath)) {
     const records = await neatCsv(csvFileContents, "utf-8");
     const metadata = yaml.load(yamlFileContents);
 
-    datasets[id] = { id, records, repositoryPath, repo, ...metadata };
+    datasets[id] = {
+      id,
+      records,
+      repositoryPath,
+      repositoryUrl,
+      repositoryName,
+      repositoryRev,
+      ...metadata,
+    };
   }
 }
