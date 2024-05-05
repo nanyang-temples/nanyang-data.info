41db62b98999ee9a052d02f11ab2cfadb248c8e5
Simon Wiles <simonjwiles@gmail.com>
2024-05-05 15:30:27 -0700

    Add "Survey of Chinese Temples in Bintan (2024)"

--- src/scripts/main-map.mjs
+++ src/scripts/main-map.mjs
@@ -11,6 +11,7 @@ const datasetURLs = [
   "../data/01-temples_in_bangkok_mb/by-latlong.json",
   "../data/01-SGPchineseAssociations/by-latlong.json",
   "../data/02-SGPtemples/by-latlong.json",
+  "../data/01-temples_in_bintan_mb/by-latlong.json",
 ];
 
 const getDataset = async (url) => {
