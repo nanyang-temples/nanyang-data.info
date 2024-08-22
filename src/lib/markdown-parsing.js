2da01820016f299dcf628f35bf593cb2ece29c71
Simon Wiles <simonjwiles@gmail.com>
2024-08-22 12:17:22 -0700

    Add credits to info boxes

--- src/lib/markdown-parsing.js
+++ src/lib/markdown-parsing.js
@@ -4,12 +4,16 @@ import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 
-export const md2html = async (markdown) => {
+export const md2html = async (markdown, remove_outer_tag = false) => {
   let processor = unified()
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeStringify, { allowDangerousHtml: true });
 
+  if (remove_outer_tag) {
+    // HACK!!
+    return String(await processor.process(markdown)).slice(3, -4);
+  }
   return String(await processor.process(markdown));
 };
